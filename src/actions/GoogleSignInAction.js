00d095f289d0aebbe2bbae8fbccbd7e6a2b50b6b
Talha javed <{jawedtalha0@gmail.com}>
2019-01-20 20:41:04 +0500

    google auth

--- src/actions/GoogleSignInAction.js
+++ src/actions/GoogleSignInAction.js
@@ -7,36 +7,42 @@ export function loginWithGoogle() {
     const TIMEOUTID = setTimeout(async () => {
       try {
         const result = await Expo.Google.logInAsync({
-          behavior: "web",
           androidClientId:
-            "223372639224-gd9fk1qq044qvt8790k742cta8sgr1qq.apps.googleusercontent.com",
+            '187936954064-1d43h6fn26a55cmj08gjn2homvlm4pv8.apps.googleusercontent.com',
           iosClientId:
-            "223372639224-s2qibpht817plkrd8tcv3kkhil4ksk2v.apps.googleusercontent.com",
-          androidStandaloneAppClientId:
-            "223372639224-7sh1eagh0mhsb3lhv27s74l5o2emue54.apps.googleusercontent.com",
-          iosStandaloneAppClientId:
-            "223372639224-uq5accrlp53objd3710jdmfnoj0qilk4.apps.googleusercontent.com",
-          scopes: ["profile", "email"]
+            '187936954064-4aftsohm5fu6kb3cgk9dsnkll9nicklj.apps.googleusercontent.com',
+          scopes: ['profile', 'email'],
         });
-        console.log("google signin result",result);  
+        console.log("google signin result", result);
         if (result.type === "success") {
-          let credential = firebase.auth.GoogleAuthProvider.credential(
-            result.idToken,
-            result.accessToken
-          );
-          const val = await firebase
+          console.log('result===', result);
+
+          const { idToken, accessToken } = result;
+          const credential = firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
+          firebase
             .auth()
-            .signInAndRetrieveDataWithCredential(credential);
-          if (val.user) {
-            dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, data: { user: val.user } });
-            const updatedProfile = await val.user.updateProfile({photoURL:result.user.photoURL,displayName:result.user.name})
-          } else if (val.uid) {
-            dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, data: { user: val } });
-          } else {
-            alert(val);
-          }
+            .signInAndRetrieveDataWithCredential(credential)
+            .then(success => {
+              // user res, create your user, do whatever you want
+              const val = success
+              if (val.user) {
+                console.log('success==>', val);
+                dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, data: { user: val.user } });
+                // const updatedProfile = await val.user.updateProfile({ photoURL: result.user.photoURL, displayName: result.user.name })
+              } else if (val.uid) {
+                dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, data: { user: val } });
+              } else {
+                alert(val);
+              }
+            })
+            .catch(error => {
+              console.log("firebase cred err:", error.message);
+            });
+          return result.accessToken;
+
         } else {
-          dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, error: "cancelled" });
+          console.log('/////');
+          return { cancelled: true };
         }
       } catch (error) {
         dispatch({ type: SIGNUP_GOOGLE_ACCOUNT, error: error.message });
