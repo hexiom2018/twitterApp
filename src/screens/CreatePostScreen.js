cc5c3eec0094715a7aed871facb687aa9f7a94f8
SyedUzairAlii <maazuzair3322@gmail.com>
2019-01-22 21:07:25 +0500

    location

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -4,7 +4,7 @@ import { Container, Picker, H1, Form, Item, CheckBox, Label, Input, Button, Text
 import { reduxForm, Field, formValueSelector } from 'redux-form';
 import { connect } from 'react-redux';
 import Expo, { Video, ImagePicker, Location, Permissions, Constants } from 'expo';
-import moment from 'moment';
+// import moment from 'moment';
 import CameraExample from '../component/CreatePosts/Camera'
 import { DrawerActions } from 'react-navigation';
 import { bindActionCreators } from 'redux';
@@ -29,6 +29,8 @@ const validate = (values) => {
 
 
 
+
+
 const MediaPicker = ({ input: { onChange, value, ...inputProps }, ...restProps, onPress }) => (
     <Item style={[{ marginLeft: 0, marginVertical: 4, borderRadius: 5 }]} >
 
@@ -42,20 +44,20 @@ const MediaPicker = ({ input: { onChange, value, ...inputProps }, ...restProps, 
 )
 
 const InputText = ({ input, label }) => (
-    <Item style={[{ marginLeft: 0, padding: 5, backgroundColor: 'white', marginVertical: 4, borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]} inlineLabel>
+    <Item style={[{  padding: 5, backgroundColor: 'white', marginVertical: 4, borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]} inlineLabel>
         <Input placeholderTextColor='#BDBEBF' style={{ color: '#737277', }} {...input} placeholder={label} />
     </Item>
 )
 
 
-const InputTextArea = ({ input, label }) => (<Item style={[{ marginLeft: 0, padding: 5, backgroundColor: 'white', marginVertical: 4, borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]} stackedLabel>
+const InputTextArea = ({ input, label }) => (<Item style={[{   justifyContent: "center", backgroundColor: 'white', borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]} stackedLabel>
 
     <Textarea {...input} rowSpan={5} style={[{ width: '100%', color: '#737277', }]} placeholder={label} placeholderTextColor='#BDBEBF' />
 </Item>)
 
 
 const renderPicker = ({ input: { onChange, value, ...inputProps }, children, ...pickerProps }) => (
-    <Item picker style={[{ marginLeft: 0, padding: 5, backgroundColor: 'white', marginVertical: 4, borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]}>
+    <Item picker style={[{ justifyContent: "center", padding: 5, backgroundColor: 'white', marginVertical: 4, borderRadius: 5, borderWidth: 3, borderColor: '#FFFEF6', }]}>
 
         <Picker
             selectedValue={value}
@@ -118,6 +120,8 @@ class CreatePostScreen extends Component {
         UploadingModalVisibile: false,
         title: null,
         des: null,
+
+        where: { lat: null, lng: null }, //current location here(nabeel ye lo)
         result: null,
     };
     static navigationOptions = {
@@ -151,71 +155,102 @@ class CreatePostScreen extends Component {
         this.setState({ result });
     };
 
-    _getLocationAsync = async () => {
-        // const { status } = await Expo.Permissions.getAsync(Expo.Permissions.LOCATION);
 
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-            this.setState({
-                errorMessage: 'Permission to access location was denied',
-            });
-            Alert.alert(
-                'Alert',
-                'Permission to access location was denied',
-                { cancelable: false }
-            )
-        }
-
-        let location = await Location.getCurrentPositionAsync({});
-        this.setState({ location });
-        console.log(location, 'your location coords')
-        this.props.change('lat', location.coords.latitude);
-        this.props.change('lng', location.coords.longitude);
-        this.props.change('date', moment.now());
-    };
-
+    //uzair
     componentDidMount() {
-
-        if (Platform.OS === 'android' && !Constants.isDevice) {
+        // this.setState({ UID: this.props.UID })
+        if (!Constants.isDevice) {
             this.setState({
                 errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
             });
+            console.log("not ios ")
         } else {
             this._getLocationAsync();
-            const { GetUserLocation } = this.props.actions
-            console.log(this.props, 'props here')
-            // GetUserLocation()
-
         }
 
-
-
-        // if (status !== 'granted') {
-        //     const { status } = await Expo.Permissions.askAsync(Expo.Permissions.LOCATION)
-        //     if (status !== 'granted') {
-        //         alert("cannot show photos on the map");
-        //     }
-        // }
-
-        // try {
-        //     const location = Expo.Location.getCurrentPositionAsync();
-        //     const { coords } = await location
-        //     console.log(coords,'cooordes heree')
-        //     // lat and long of default USA San Francisco
-        //     coords.latitude = 37.773972;
-        //     coords.longitude = -122.431297;
-
-        //     this.props.change('lat', coords.latitude);
-        //     this.props.change('lng', coords.longitude);
-        //     this.props.change('date', moment.now());
-        //     console.log('coords', coords)
-        // } catch (error) {
-        //     console.error(error)
-        //     alert("cannot find your location ")
-        // }
-
-
     }
+    _getLocationAsync = async () => {
+
+
+        let location = await Location.getCurrentPositionAsync({});
+        console.log("permission  granted ")
+
+        this.setState({
+            location,
+            where: { lat: location.coords.latitude, lng: location.coords.longitude },
+            get: true
+        });
+        console.log("location===>>>>", location)
+
+    };
+
+    //uzair
+
+    // _getLocationAsync = async () => {
+    //     // const { status } = await Expo.Permissions.getAsync(Expo.Permissions.LOCATION);
+
+    //     let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    //     if (status !== 'granted') {
+    //         this.setState({
+    //             errorMessage: 'Permission to access location was denied',
+    //         });
+    //         Alert.alert(
+    //             'Alert',
+    //             'Permission to access location was denied',
+    //             { cancelable: false }
+    //         )
+    //     }
+
+    //     let location = await Location.getCurrentPositionAsync({});
+    //     this.setState({ location });
+    //     console.log(location, 'your location coords')
+    //     this.props.change('lat', location.coords.latitude);
+    //     this.props.change('lng', location.coords.longitude);
+    //     this.props.change('date', moment.now());
+    // };
+
+    // componentDidMount() {
+
+    // if (Platform.OS === 'android' && !Constants.isDevice) {
+    //     this.setState({
+    //         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+    //     });
+    // } else {
+    //     this._getLocationAsync();
+    //     const { GetUserLocation } = this.props.actions
+    //     console.log(this.props, 'props here')
+    //     // GetUserLocation()
+
+    // }
+
+
+
+    // if (status !== 'granted') {
+    //     const { status } = await Expo.Permissions.askAsync(Expo.Permissions.LOCATION)
+    //     if (status !== 'granted') {
+    //         alert("cannot show photos on the map");
+    //     }
+    // }
+
+    // try {
+    //     const location = Expo.Location.getCurrentPositionAsync();
+    //     const { coords } = await location
+    //     console.log(coords,'cooordes heree')
+    //     // lat and long of default USA San Francisco
+    //     coords.latitude = 37.773972;
+    //     coords.longitude = -122.431297;
+
+    //     this.props.change('lat', coords.latitude);
+    //     this.props.change('lng', coords.longitude);
+    //     this.props.change('date', moment.now());
+    //     console.log('coords', coords)
+    // } catch (error) {
+    //     console.error(error)
+    //     alert("cannot find your location ")
+    // }
+
+
+    // }
 
     async  mediaPicker(onChange) {
         try {
@@ -313,11 +348,13 @@ class CreatePostScreen extends Component {
                     <View style={{ flex: 1 }}>
                     </View>
 
-                    <Form style={{ padding: 0, marginLeft: 0, }}>
+                    <Form style={{ justifyContent: "center",width: '100%' }}>
                         {
-                            <View style={{ marginTop: 10 ,height:250,width:500}}>
-                                <CameraExample />
+                            <View style={{ marginTop: 10, height: 250, width: '100%' }}>
+
+                                    <CameraExample  style={{ width: "100%" }} />
                             </View>
+
                         }
                         {
                             !this.showSelectedImage(this.props.mediaPicker) && (<Field name='mediaPicker' component={MediaPicker} label='mediaPicker' onPress={this.mediaPicker} />)
