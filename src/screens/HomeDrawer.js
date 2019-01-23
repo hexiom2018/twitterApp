3341481b65edb7a8e53c114de93ef80498b1784d
Talha-Jawed <jawedtalha0@gmail.com>
2019-01-23 06:47:08 -0800

    log out

--- src/screens/HomeDrawer.js
+++ src/screens/HomeDrawer.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity, Text, Clipboard, SafeAreaView } from 'react-native';
-import { DrawerItems } from 'react-navigation';
+import { DrawerItems, StackActions, NavigationActions } from 'react-navigation';
 import { View } from 'native-base';
 import firebase from "firebase";
 import { connect } from "react-redux";
 
 
 class HomeDrawer extends Component {
+    logout() {
+        firebase.auth().signOut()
+        const resetAction = StackActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({ routeName: 'Register' }),
+            ]
+        })
+        this.props.navigation.dispatch(resetAction)
+        this.props.logout()
 
+    }
 
     render() {
 
@@ -17,20 +28,19 @@ class HomeDrawer extends Component {
                 <DrawerItems {...this.props} />
                 <View style={styles.settingsContainer}>
                     <TouchableOpacity onPress={() => { this.props.navigation.navigate('ContactUs') }}>
-                        <Text style={styles.itemText}>contact us</Text>
+                        <Text style={styles.itemText}>Contact us</Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => { this.props.navigation.navigate('AboutUs') }}>
-                        <Text style={styles.itemText}>about</Text>
+                        <Text style={styles.itemText}>About</Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => { this.props.navigation.navigate('Privacy') }}>
-                        <Text style={styles.itemText}>privacy</Text>
+                        <Text style={styles.itemText}>Privacy</Text>
                     </TouchableOpacity>
-
                     <TouchableOpacity onPress={() => { Clipboard.setString('https://www.live.com') }}>
                         <Text style={styles.itemText}>Rate us!!</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { firebase.auth().signOut(); this.props.logout() }}>
-                        <Text style={[styles.itemText, { color: "#fff" }]}>Log Out</Text>
+                    <TouchableOpacity onPress={() => this.logout()}>
+                        <Text style={[styles.itemText, { color: 'red' }]}>Log Out</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -49,20 +59,21 @@ export default connect(() => { return {} }, mapDispatchToProps)(HomeDrawer);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 24,
+        // paddingTop: 24,
     },
     settingsContainer: {
         position: 'absolute',
-        bottom: 0,
+        bottom: 275,
         left: 0,
         right: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 16,
+        // top: 25,
+        // paddingTop: 140,
+        paddingLeft: 18
     },
     itemText: {
         fontWeight: 'bold',
-        fontSize: 16,
+        fontSize: 14,
+        marginTop: 30
     },
 
 })
